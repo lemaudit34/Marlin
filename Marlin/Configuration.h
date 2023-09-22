7dbb004f3d1d6242b98ad9e8ecf470025b32ce84
lemaudit34 <141411226+lemaudit34@users.noreply.github.com>
2023-09-22 18:44:03 +0200

    steps moteur adjustement

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1222,7 +1222,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.2, 80.2, 399.6, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.99, 79.72, 399.6, 95 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
