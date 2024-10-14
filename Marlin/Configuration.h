0dc7d2aeb89a6efc60e7ca0780ea4785787fd8a4
lemaudit34 <141411226+lemaudit34@users.noreply.github.com>
2024-10-14 15:57:04 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(V2.1.3, Havez config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(V2.1.4, Havez config)" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
